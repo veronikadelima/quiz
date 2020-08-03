06e59646445bdf0bd530301801df81c05438ef8b
Veronica de Lima <veronikadelima@gmail.com>
2020-08-03 18:25:41 -0300

    Exercicio Quiz
    Iniciando o Exercicio Quiz em 03/08/2020

---
+++ README.md
@@ -0,0 +1,3 @@
+Exercício - Quiz
+
+- Iniciando o Exercício Quiz em 03/08/2020.
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require "config.php"
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    
+    <title>One Page</title>
+    
+    <link rel="stylesheet" href="<?=$base;?>/assets/css/style.css"/> 
+    <link rel="stylesheet" href="<?=$base;?>/assets/css/bootstrap.css"/>
+
+    <script src="<?=$base;?>/assets/js/jquery-3.5.1.min.js"></script>
+    <script src="<?=$base;?>/assets/js/bootstrap.bundle.js"></script>
+       
+</head>
+<body>
+
+
+   
+</body>
+</html>
